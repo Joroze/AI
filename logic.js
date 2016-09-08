c924ab058686dcdc0df40311ad0273d4de822283
Jordan <Joroze@users.noreply.github.com>
2016-09-08 13:47:41 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -39,3 +39,26 @@ if ($(this).hasClass('white'))
 		$(this).css("background-color","#EB0955");
   }
 });
+
+/**
+  Bootstrap Alerts -
+  Function Name - showalert()
+  Inputs - message,alerttype
+  Example - showalert("Invalid Login","alert-error")
+  Types of alerts -- "alert-error","alert-success","alert-info"
+  Required - You only need to add a alert_placeholder div in your html page wherever you want to display these alerts
+
+  function showalert(message,alerttype) {
+
+    $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
+
+    setTimeout(function() { // this will automatically close the alert and remove this if the users doesnt close it in 5 secs
+
+
+      $("#alertdiv").remove();
+
+    }, 5000);
+  }
+
+
+});
