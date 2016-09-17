d72ebd1421ec5d13b0285c9c6781cc15cd7083b9
Jordan <Joroze@users.noreply.github.com>
2016-09-17 19:08:36 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,47 @@
 var table;
 
+var Cell = (function() {
+    // "private" variables 
+    //var color;
+  
+    //var row;
+    //var col;
+
+    // constructor
+    function Cell(x,y,color) {
+      this._row = x;
+      this._col = y;
+      this._color = color;
+      
+      //alert("Cell created: " + color + " " + "(" + row + ", " + col + ")");
+    };
+
+    // add the methods to the prototype so that all of the 
+    // Foo instances can access the private static
+    Cell.prototype.getRow = function() {
+      return this._row;
+    }
+    
+    Cell.prototype.getCol = function() {
+      return this._col;
+    }
+  
+    Cell.prototype.getColor = function() {
+        return this._color;
+    };
+    Cell.prototype.setColor = function(color) {
+        this._color = color;
+    };
+  
+    Cell.prototype.toString = function()
+    {
+        return this._color;
+    }
+
+    return Cell;
+})();
+
+
   function showalert(message,alerttype) {
 
     $('#alert_placeholder').append('<div id="alertdiv" class="alert ' +  alerttype + '"><a class="close" data-dismiss="alert">×</a><span>'+message+'</span></div>')
@@ -40,12 +82,30 @@ else
 }
 concentration = $("#concentration-field").val();
 
+  
+  var myTableArray = [];
+  var color;
+  
+  for (var i = 0; i < rows; i++) {
+    myTableArray[i] = [];
+    for (var j = 0; j < cols; j++) {
+      var random = Math.floor((Math.random() * 100) + 1);
+        if (concentration < random) {
+            color = "white";
+        } else if (concentration > random) {
+            color = "black";
+        }
+      
+            myTableArray[i][j] = new Cell(i, j, color);
+    }
+}
+  
 table = document.getElementById("table");
 for (var i = 0; i < rows; i++) {
     var tr = document.createElement('tr');
     for (var j = 0; j < cols; j++) {
         var td = document.createElement('td');
-        if (concentration < Math.floor((Math.random() * 100) + 1)) {
+        if (myTableArray[i][j].getColor() == "white") {
             td.className = "white";
         } else {
             td.className = "black";
@@ -57,6 +117,31 @@ for (var i = 0; i < rows; i++) {
 document.body.appendChild(table);
 
 
+
+  
+/*$("#table tr").each(function() { 
+    var arrayOfThisRow = [];
+    var tableData = $(this).find('td');
+    if (tableData.length > 0) {
+        tableData.each(function() {
+          if( $(this).hasClass("white"))
+             {
+             arrayOfThisRow.push("white");
+             }
+          else if( $(this).hasClass("black"))
+             {
+             arrayOfThisRow.push("black");
+             }
+           });
+        myTableArray.push(arrayOfThisRow);
+    }
+});
+*/
+
+alert(myTableArray); // alerts the entire array
+
+//alert(myTableArray[1][0]); // Alerts the first tabledata of the first tablerow
+  
 }
 
 
@@ -71,4 +156,8 @@ if ($(this).hasClass('white'))
 
   }
 });
+
+
+
+
 
